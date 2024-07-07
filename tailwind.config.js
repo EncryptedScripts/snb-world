0a9e2b0a7040a8a8ca1dcb2c874c04cd4413ea2f
Yunan Yuansyah <yunanyuansyah@gmail.com>
2024-07-07 09:32:55 +0700

    First Commit

---
+++ tailwind.config.js
@@ -0,0 +1,29 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/**/*.{html,js,vue,jsx,ts,tsx}"],
+  theme: {
+    
+    fontFamily: {
+      'saira': ['"Saira Semi Condensed"', 'sans-serif'],
+    },
+    extend: {
+      width  : {
+        'btn': '80px',
+        'btn-lg': '120px',
+        'btn-xl': '160px', 
+        'btn-pfl': '23px',
+        'btn-pfl-lg': '30px',
+      },
+
+      height  : {
+        'btn': '40px',
+      },
+
+      padding : {
+        '4': '0.75rem',
+        '5': '1rem',
+      },
+    }
+  },
+  plugins: [],
+}
